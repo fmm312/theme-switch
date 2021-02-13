64528a2e407eda2dcf40b4b737c7e94ab098acce
Felipe <fmm312@gmail.com>
2021-02-13 20:37:00 -0300

    Creating a functionality for theme switch

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#333333',
+        secundary: '#c62e65',
+
+        background: '#222222',
+        text: '#ffffff'
+    }
+}
